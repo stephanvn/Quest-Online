631dbc2d90a43d2da33ed2f5d32ad5d3e5bb234a
Stefferp <steffanmitrovic@live.nl>
2016-03-02 23:13:06 +0100

    Character move, camera move, started collisions

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,29 +6,25 @@ public class CameraController : MonoBehaviour {
     public float interpVelocity;
     public float minDistance;
     public float followDistance;
-    public GameObject target;
     public Vector3 offset;
     Vector3 targetPos;
+
+    Camera mycam;
+    public Transform target;
+
     // Use this for initialization
     void Start()
     {
-        targetPos = transform.position;
+        mycam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        //mycam.orthographicSize = (Screen.height / 100f) / 4f;
         if (target)
         {
-            Vector3 posNoZ = transform.position;
-            posNoZ.z = target.transform.position.z;
-
-            Vector3 targetDirection = (target.transform.position - posNoZ);
-
-            interpVelocity = targetDirection.magnitude * 5f;
-            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
-            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
-
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3(0, 0, -10); ;
         }
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rigid.MovePosition(rigid.position + movement_vector * 2 * Time.deltaTime);
+        rigid.MovePosition(rigid.position + movement_vector * Time.deltaTime/2);
         // if (movement_vector != Vector2.zero)
        // {
        //     anim.SetBool("is_walking", true);
