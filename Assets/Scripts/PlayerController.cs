00b93c5694974abb599b49007d7f46e63f0b6ec0
Brian Casson <brian_casson@hotmail.com>
2016-03-03 20:50:10 +0100

    No diagonal movement (shitty fix)
    Je kan niet meer diagonaal lopen, maar de animaties voor horizontaal
    lopen werken niet. Hij doet het wel als je begint met horizontaal lopen,
    maar als je 1x verticaal hebt gelopen, werken de animaties voor
    horizontaal niet meer.
    Ik heb er een tijd naar gekeken, maar kom er nu ff niet uit.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,19 +14,45 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (movement_vector != Vector2.zero)
+        //Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //if (movement_vector != Vector2.zero)
+        //{
+        //    anim.SetBool("iswalking", true);
+        //    anim.speed = 0.8f;
+        //    anim.SetFloat("input_x", movement_vector.x);
+        //    anim.SetFloat("input_y", movement_vector.y);
+        //}
+        //else
+        //{
+        //    anim.SetBool("iswalking", false);
+        //    anim.speed = .2f;
+        //}
+        //rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime);
+
+        Vector2 movement_horizontal = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        Vector2 movement_vertical = new Vector2(0, Input.GetAxisRaw("Vertical"));
+
+        if (movement_horizontal != Vector2.zero)
         {
             anim.SetBool("iswalking", true);
             anim.speed = 0.8f;
-            anim.SetFloat("input_x", movement_vector.x);
-            anim.SetFloat("input_y", movement_vector.y);
+            anim.SetFloat("input_x", movement_horizontal.x);
+            rbody.MovePosition(rbody.position + movement_horizontal * Time.deltaTime);
+        }
+        if (movement_vertical != Vector2.zero)
+        {
+            anim.SetBool("iswalking", true);
+            anim.speed = 0.8f;
+            anim.SetFloat("input_y", movement_vertical.y);
+            rbody.MovePosition(rbody.position + movement_vertical * Time.deltaTime);
         }
         else
         {
             anim.SetBool("iswalking", false);
             anim.speed = .2f;
         }
-        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime);
+        
+        
+
     }
 }
