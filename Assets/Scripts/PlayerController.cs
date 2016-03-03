625b1c30e919c558d0a0c4bc393c077df0ae9df7
Brian Casson <brian_casson@hotmail.com>
2016-03-03 14:24:57 +0100

    Animations added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody;
-    //Animator anim;
+    Animator anim;
 
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -17,13 +17,13 @@ public class PlayerController : MonoBehaviour
         Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         if (movement_vector != Vector2.zero)
         {
-            //anim.SetBool("iswalking", true);
-            //anim.SetFloat("input_x", movement_vector.x);
-            //anim.SetFloat("input_y", movement_vector.y);
+            anim.SetBool("iswalking", true);
+            anim.SetFloat("input_x", movement_vector.x);
+            anim.SetFloat("input_y", movement_vector.y);
         }
         else
         {
-            //anim.SetBool("iswalking", false);
+            anim.SetBool("iswalking", false);
         }
         rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime);
     }
