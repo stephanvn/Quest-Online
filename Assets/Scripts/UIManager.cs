8e476f870257a8afd9f75ae37cb63467c536bd23
BrianCasson <brian_casson@hotmail.com>
2016-03-09 15:58:42 +0100

    Added Menu UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public Text username;
+	public Text level;
+	public Text hp;
+	public string playerName;
+	public int playerLvl;
+	public int playerHP;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hp.text = "HP: " + playerHP;
+		username.text = playerName;
+		level.text = "Level: " + playerLvl;
+	}
+}
