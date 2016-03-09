b371d15825e3ffbec3cedf2418408f0394df0769
Stefferp <steffanmitrovic@live.nl>
2016-03-09 15:24:07 +0100

    Removed unused code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -44,29 +44,6 @@ public class PlayerController : MonoBehaviour
                 showInventory = true;
             }
         }
-
-        /*Vector2 movement_horizontal = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-        Vector2 movement_vertical = new Vector2(0, Input.GetAxisRaw("Vertical"));
-
-        if (movement_horizontal != Vector2.zero)
-        {
-            anim.SetBool("iswalking", true);
-            anim.speed = 0.8f;
-            anim.SetFloat("input_x", movement_horizontal.x);
-            rbody.MovePosition(rbody.position + movement_horizontal * Time.deltaTime);
-        }
-        if (movement_vertical != Vector2.zero)
-        {
-            anim.SetBool("iswalking", true);
-            anim.speed = 0.8f;
-            anim.SetFloat("input_y", movement_vertical.y);
-            rbody.MovePosition(rbody.position + movement_vertical * Time.deltaTime);
-        }
-        else
-        {
-            anim.SetBool("iswalking", false);
-            anim.speed = .2f;
-        }*/
     }
 
     void OnTriggerEnter2D(Collider2D other)
